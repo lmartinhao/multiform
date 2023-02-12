851968b11e617b23033a2c0897d0a8311a931f95
lola martinhao <lola.martinhao@gmail.com>
2023-02-11 23:28:05 -0300

    step1 page behavior finished

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,19 +1,29 @@
 import { useNavigate } from 'react-router-dom'
+import { useForm, FormActions } from '../../contexts/FormContext'
 
 import { Theme } from '../../components/Theme'
 import { StepOneContainer } from './styles'
+import { ChangeEvent } from 'react'
 
 export function FormStep1() {
   const navigate = useNavigate()
+  const { state, dispatch } = useForm()
 
   function handleNextStep() {
     navigate('/step2')
   }
 
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    })
+  }
+
   return (
     <Theme>
       <StepOneContainer>
-        <p>Passo 1/3</p>
+        <p>Passo 1/3 - {state.name}</p>
         <h1>Vamos começar com o seu nome</h1>
         <p>Preencha o campo abaixo com o seu nome completo.</p>
 
@@ -21,7 +31,12 @@ export function FormStep1() {
 
         <label>
           Seu nome completo
-          <input type="text" autoFocus />
+          <input
+            type="text"
+            autoFocus
+            value={state.name}
+            onChange={handleNameChange}
+          />
         </label>
 
         <button onClick={handleNextStep}>Próximo</button>
