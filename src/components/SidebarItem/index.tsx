c924b689c9c016677665c17437d143cf5cb5157c
lola martinhao <lola.martinhao@gmail.com>
2023-02-12 01:15:28 -0300

    sidebar created

---
+++ src/components/SidebarItem/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import * as C from './styles'
+
+type Props = {
+  title: string
+  description: string
+  icon: string
+  path: string
+}
+
+export function SidebarItem({ title, description, icon, path }: Props) {
+  return (
+    <C.Container>
+      <Link to={path}>
+        <C.Info>
+          <C.Title>{title}</C.Title>
+          <C.Description>{description}</C.Description>
+        </C.Info>
+        <C.IconArea>{icon}</C.IconArea>
+        <C.Point></C.Point>
+      </Link>
+    </C.Container>
+  )
+}
--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { Header } from '../Header'
+import { SidebarItem } from '../SidebarItem'
 import * as C from './styles'
 
 type Props = {
@@ -13,7 +14,26 @@ export function Theme({ children }: Props) {
         <Header />
 
         <C.Steps>
-          <C.Sidebar>...</C.Sidebar>
+          <C.Sidebar>
+            <SidebarItem
+              title="Pessoal"
+              description="Se identifique"
+              icon="profile"
+              path="/"
+            />
+            <SidebarItem
+              title="Profissional"
+              description="Seu nível"
+              icon="book"
+              path="/step2"
+            />
+            <SidebarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="mail"
+              path="/step3"
+            />
+          </C.Sidebar>
           <C.Page>{children}</C.Page>
         </C.Steps>
       </C.Area>
